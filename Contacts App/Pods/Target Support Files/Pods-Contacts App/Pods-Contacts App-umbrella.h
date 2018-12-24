320c72517f5e4bf40020bf7fe9bfe837b7383958
Anderson Gralha <andersongralha@gmail.com>
2018-12-24 21:04:48 -0200

    Renames project

--- Contacts App/Pods/Target Support Files/Pods-Contacts App/Pods-Contacts App-umbrella.h
+++ Contacts App/Pods/Target Support Files/Pods-Contacts App/Pods-Contacts App-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_Calendar_AppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Calendar_AppVersionString[];
+FOUNDATION_EXPORT double Pods_Contacts_AppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Contacts_AppVersionString[];
 
